34f4f6d50ae1205f90d05b8c66745468a6916e79
SSSSJIN <shoes90@naver.com>
2025-03-24 21:33:57 +0900

    0324_SJF_Scheduling_Algorithm

---
+++ Process.java
@@ -0,0 +1,20 @@
+class Process {
+
+    int pid;
+
+    int burstTime;
+
+    int waitingTime;
+
+    int turnaroundTime;
+
+
+    public Process(int pid, int burstTime) {
+
+        this.pid = pid;
+
+        this.burstTime = burstTime;
+
+    }
+
+}
